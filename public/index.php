3ecdbf7105ee6923406f77b54c4c6350d30b48bc
kyb2306553 <kyb2306553@student.ctu.edu.vn>
2026-03-24 12:03:06 +0700

    upd root index

--- public/index.php
+++ public/index.php
@@ -2,7 +2,38 @@
 
 require_once __DIR__ . '/bootstrap.php';
 
-use App\Controllers\HomeCtrl;
+use App\Controllers\HomeController;
+use App\Controllers\AdminController;
 
-$homeCtrl = new HomeCtrl();
-$homeCtrl->index();
+$url = $_GET['url'] ?? ''; 
+$url = rtrim($url, '/');
+$parts = explode('/', $url);
+
+if (!empty($parts[0]) && $parts[0] === 'admin') {
+    
+    $adminController = new AdminController();
+    $action = $parts[1] ?? 'index';
+    $id = $parts[2] ?? null; 
+
+    switch ($action) {
+        case 'index':
+            $adminController->index();
+            break;
+        case 'create':
+            $adminController->create();
+            break;
+        case 'edit':
+            $adminController->edit($id);
+            break;
+        case 'delete':
+            $adminController->delete($id);
+            break;
+        default:
+            $adminController->index();
+            break;
+    }
+    exit(); 
+}
+
+$homeController = new HomeController();
+$homeController->index();
